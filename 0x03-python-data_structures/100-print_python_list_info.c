858db9d64782e0482c19d549174e5e9273df5712
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-05-13 15:09:17 -0700

    Advancedtask

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,6 @@
-#include <Python.h>
+#include "Python.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
  * print_python_list_info - prints information about a python list object
  * @p: pointer to generic PyObject which is of PyListObject type
@@ -9,17 +9,19 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	long int size = PyList_Size(p);
-	int i = 0;
-	const char *ele_type;
-	PyListObject *obj = (PyListObject *)p;
+	PyListObject *py_list = NULL;
+	ssize_t list_len = 0;
+	ssize_t i = 0;
+	const char *ele_type = NULL;
 
-	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->allocated);
-	while (i < size)
+	list_len = PyList_Size(p);
+	py_list = (PyListObject *)p;
+	printf("[*] Size of the Python List = %ld\n", list_len);
+	printf("[*] Allocated = %ld\n", (signed long)(py_list->allocated));
+	while (i < list_len)
 	{
-		ele_type = Py_TYPE(obj->ob_item[i])->tp_name
-		printf("Element %i: %s\n", i, ele_type);
+		ele_type = Py_TYPE(py_list->ob_item[i])->tp_name;
+		printf("Element %ld: %s\n", i, ele_type);
 		i++;
 	}
 }
