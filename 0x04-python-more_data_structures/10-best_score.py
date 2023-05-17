ed285474c2375e2cd0b73b9a68f52dfc67512c57
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-05-17 10:42:05 -0700

    structure

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        sorted_items = sorted(a_dictionary.items(), key=lambda item: item[1], reverse=True)
-        for key, value in sorted_items:
-            return key
+    if a_dictionary is None:
+        return None
+    sorted_items = sorted(a_dictionary.items(), key=lambda item: item[1], reverse=True)
+    for key, value in sorted_items:
+        return key
