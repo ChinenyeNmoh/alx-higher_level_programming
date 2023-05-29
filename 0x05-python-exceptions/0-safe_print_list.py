d4c68431d7607096b1d42e11d98d44fe6f577c22
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-05-29 09:29:53 -0700

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         for i in my_list[0:x]:
             count += 1
-            print("{:d}".format(i),end='')
+            print("{:d}".format(i), end='')
         print()
         return count
     except Exception:
