bf125d7bd7edf96e4f61993fae264843a17773ca
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-06-05 04:12:09 -0700

    moreclasses

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
         """sets the new width value"""
         if type(value) != int:
             raise TypeError('width must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -39,7 +39,7 @@ class Rectangle:
         """sets the new height value"""
         if type(value) != int:
             raise TypeError('height must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
