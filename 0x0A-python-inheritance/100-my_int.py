ef6659932f6f8f07eb489cf63ec46509f374891c
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-06-10 05:16:54 -0700

    inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,13 +2,12 @@
 
 
 class MyInt(int):
-    """inversed int, overriding operator methods / comparison methods
-    """
+    """overriding operator methods / comparison methods"""
 
-    def __eq__(self, other):
-        """ changes == to !="""
-        return (int(self) != other)
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
-    def __ne__(self, other):
-        """ changes != to =="""
-        return (int(self) == other)
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
