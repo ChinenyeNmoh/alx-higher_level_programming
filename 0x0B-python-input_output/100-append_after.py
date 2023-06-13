6b452832bc0ae23c490de6afda146df880d7eafb
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-06-13 15:23:05 -0700

    filehandling

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,11 +8,13 @@ def append_after(filename="", search_string="", new_string=""):
     """appends "new_string" after a line containing
     "search_string" in "filename" """
     with open(filename, 'r') as f:
-        new_list = []
-        while True:
-            line = f.read()
-            new_list.append(line)
-            if search_string in line:
-                new_list.append(new_string)
+        lines = f.readlines()
+
+    new_list = []
+    for line in lines:
+        new_list.append(line)
+        if search_string in line:
+            new_list.append(new_string)
+
     with open(filename, 'w') as f:
         f.writelines(new_list)
