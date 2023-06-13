92637d1d5b3e9ce13a54ec2a7b42244d1050c6f1
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-06-13 13:57:49 -0700

    filehandling

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -17,9 +17,9 @@ class Student:
                 all(type(i) == str for i in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
- 
+
     def reload_from_json(self, json):
         """Replace all attributes of the Student"""
         for key in json:
             value = json[key]
-             setattr(self, key, value)
+            setattr(self, key, value)
