ef24f35f9038ca0e2a688c6f961e3d61eab7fc73
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-06-13 12:47:10 -0700

    filehandling

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Defines a Python class-to-JSON function."""
 
-import json
-
 
 def class_to_json(obj):
     """Return the dictionary represntation of a simple data structure."""
-    json_data = json.dumps(obj.__dict__)
+    json_data = obj.__dict__
     return json_data
