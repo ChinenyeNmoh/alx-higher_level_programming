327eca876f27c265ef0a14787f153a4490fefc56
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-08-14 08:18:12 -0700

    javascript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,12 @@
 const result = process.argv.slice(2);
 if (result.length <= 1) {
   console.log('0');
-} else{
-  convertedarray = [];
-for (let i = 0; i < result.length; i++) {
-  const convertednums = +result[i];
-  convertedarray.push(convertednums);
-}
-convertedarray.sort((a, b) => b - a);
-console.log(convertedarray[1]);
+} else {
+  const convertedarray = [];
+  for (let i = 0; i < result.length; i++) {
+    const convertednums = +result[i];
+    convertedarray.push(convertednums);
+  }
+  convertedarray.sort((a, b) => b - a);
+  console.log(convertedarray[1]);
 }
