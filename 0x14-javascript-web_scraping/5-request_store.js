591fc92f86e9feca7132009d456417dd44a8736c
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-09-26 12:16:01 +0000

    web

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filename = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  fs.writeFile(filename, body, (error) => {
+    if (error) console.log(err);
+  });
+});i
